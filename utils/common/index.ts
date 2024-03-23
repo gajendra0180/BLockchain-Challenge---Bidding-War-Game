4e1424a9a00dd0627b9132ad6a8f590df5def440
Gajendra <gajendra@magpiexyz.io>
2024-03-23 17:05:33 +0530

    remove unnecessary files

--- utils/common/index.ts
+++ utils/common/index.ts
@@ -1,12 +1,2 @@
 export { ether, gWei, bitcoin, usdc } from "./unitsUtils";
-export {
-  divDown,
-  min,
-  preciseDiv,
-  preciseDivCeil,
-  preciseMul,
-  preciseMulCeil,
-  preciseMulCeilInt,
-  preciseDivCeilInt
-} from "./mathUtils";
 export { Blockchain } from "./blockchainUtils";
